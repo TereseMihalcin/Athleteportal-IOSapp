b4774e87cc2ec75892ca4c50b22c2a282f3dac9a
Dan Williams <dmwilly9@gmail.com>
2021-03-20 11:26:21 -0400

    Initial Commit to create XCode project

---
+++ AthletePortal/AthletePortal/AthletePortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthletePortalApp.swift
+//  AthletePortal
+//
+//  Created by Daniel Williams on 17/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AthletePortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AthletePortal/AthletePortal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AthletePortal
+//
+//  Created by Daniel Williams on 17/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Test, this is the iOS Athlete Scheduling app!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
